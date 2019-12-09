d81ab49ecd5c10767a7d8b2425802757849ebd4e
Anne Sofie <AnnesofieKolbak@outlook.com>
2019-12-09 14:00:57 +0100

    Login and create + delete account

---
+++ JS/LoginMenubar.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    setLogin();
+};
+
+function setLogin(){
+    document.getElementById("login").innerHTML = "";
+    if(sessionStorage.getItem("token") == null)
+    {
+        document.getElementById("login").innerHTML = "Log in";
+    }
+    else
+    {
+        document.getElementById("login").innerHTML = sessionStorage.getItem("name");
+    }
+}
+
+function LoginPath(){
+    if(sessionStorage.getItem("token") == null)
+    {
+        alert("Token er null");
+        location.href = "LoginPage.html";
+    }
+    else
+    {
+        location.href = "AccountPage.html"
+    }
+}
