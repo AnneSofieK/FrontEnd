f6963a90b5a9d9554031b9dbebd3e841cf9c7a2d
sbog8 <shareni.ortega@udem.edu>
2019-12-10 11:31:55 +0100

    Fetch information from session

--- JS/Finish.js
+++ JS/Finish.js
@@ -8,5 +8,32 @@ $(document).ready(function () {
 
     let reasonCode = document.getElementById("reasonCode");
     reasonCode.innerText = sessionStorage.getItem("reasonCode");
+
+    let driverName = document.getElementById("driverName");
+    driverName.innerText = sessionStorage.getItem("driverName");
     
+    let driverSurname = document.getElementById("driverSurname");
+    driverSurname.innerText = sessionStorage.getItem("driverSurname");
+
+    let address = document.getElementById("address");
+    address.innerText = sessionStorage.getItem("address");
+
+    let postCode = document.getElementById("postCode");
+    postCode.innerText = sessionStorage.getItem("postCode");
+
+    let city = document.getElementById("city");
+    city.innerText = sessionStorage.getItem("city");
+
+    let email = document.getElementById("email");
+    email.innerText = sessionStorage.getItem("email");
 });
+
+function send_Claim() {
+    let checkAccount = document.getElementById("account-check");
+    if(checkAccount.checked == true){
+        location.href = "CreateAccountPage.html";
+    }
+    else{
+        location.href = "ClaimSent.html";
+    }
+}
